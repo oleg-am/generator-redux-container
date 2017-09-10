3f5874a28596abe642079d1cac1be8ce049c1915
Oleg Avgustinov <avgustinov.oleg@gmail.com>
2017-09-10 21:45:57 +0300

    add basic questions

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,76 @@
-'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const _ = require('lodash');
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the tremendous ' + chalk.red('generator-redux-container') + ' generator!'
+      `Welcome to the ${chalk.red('redux-container')} generator!`,
     ));
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    // console.log('this.config: ', this.config);
 
-    return this.prompt(prompts).then(props => {
+    const LAYOUTS = ['default', 'withFilter', 'modal'];
+    const FEATURES = ['validation', 'GriddleTable'];
+
+    const prompts = [
+      // {
+      //   type: 'confirm',
+      //   name: 'someAnswer',
+      //   message: 'Would you like to enable this option?',
+      //   default: true,
+      // },
+      {
+        type: 'input',
+        name: 'containerName',
+        message: 'What`s Container name:',
+        default: 'ContainerName',
+      },
+      {
+        type: 'input',
+        name: 'mainReducer',
+        message: 'What`s main reducer name:',
+        default({ containerName }) {
+          return _.camelCase(containerName);
+        },
+      },
+      {
+        type: 'input',
+        name: 'dataReducer',
+        message: 'What`s child reducer name for data:',
+        default: 'data',
+      },
+      {
+        type: 'list',
+        name: 'layout',
+        message: 'What`s layout:',
+        choices: LAYOUTS,
+      },
+      {
+        type: 'checkbox',
+        name: 'features',
+        message: 'What features do you want to include:',
+        default: [],
+        choices() {
+          return FEATURES;
+        },
+
+      },
+    ];
+
+    return this.prompt(prompts).then((props) => {
+      console.log('props: ', props);
       // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
-
-  install() {
-    this.installDependencies();
-  }
+  // writing() {
+  //   this.fs.copy(
+  //     this.templatePath('dummyfile.txt'),
+  //     this.destinationPath('dummyfile.txt')
+  //   );
+  // }
 };
